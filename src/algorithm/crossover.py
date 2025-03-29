559dec5ed5604aadecb33936a95824a515e76f27
leosalgado <leonardosalgado@protonmail.com>
2025-03-28 23:35:34 -0300

    Simplify crossover function

--- src/algorithm/crossover.py
+++ src/algorithm/crossover.py
@@ -1,16 +1,11 @@
 from utils import *
 
 def crossover(parents, days):
-  children = np.zeros_like(parents)
-  crossover_probability = random.random_sample()
+  children = parents.copy()
 
-  if(crossover_probability < 0.8):
+  if random.random_sample() < 0.8: # Crossover probability
     cut = random.randint(1, days)
-    children[0][:cut] = parents[0][:cut]
-    children[0][cut:] = parents[1][cut:]
-    children[1][cut:] = parents[0][cut:]
-    children[1][:cut] = parents[1][:cut]
-  else:
-    children[0] = parents[0]
-    children[1] = parents[1]
+    children[0][:cut], children[1][:cut] = parents[0][:cut], parents[1][:cut]
+    children[0][cut:], children[1][cut:] = parents[1][cut:], parents[0][cut:]
+
   return children
